2bc24774441ef5e7c85fb7cc9abb4d5add8e11ee
Tseing <im.yczeng@foxmail.com>
2025-09-06 22:14:44 +0800

    refactor: 移除浮动图片并上移词卡

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="bg-rosybrown-800 h-56 rounded-t-[48px] text-center">
-    <div class="flex flex-col pt-20">
-      <span class="text-sans text-rosybrown-50 mb-2"
-        >© 2025 米时典 SeeDict
-      </span>
-      <div v-if="showBeian" class="text-rosybrown-400">
-        <a href="beian.miit.gov.cn" target="_blank"
-          >闽 ICP 备 2025111999 号 - 1</a
-        >
+  <div class="bg-wheat-50">
+    <div class="bg-rosybrown-800 h-56 rounded-t-[48px] text-center">
+      <div class="flex flex-col pt-20">
+        <span class="text-sans text-rosybrown-50 mb-2"
+          >© 2025 米时典 SeeDict
+        </span>
+        <div v-if="showBeian" class="text-rosybrown-400">
+          <a href="beian.miit.gov.cn" target="_blank"
+            >闽 ICP 备 2025111999 号 - 1</a
+          >
+        </div>
       </div>
     </div>
   </div>
