ef0f9f1b39d9bf0fe3a41e75673404b922489b00
Tseing <im.yczeng@foxmail.com>
2025-09-08 22:13:55 +0800

    feat: 添加 About 页面

---
+++ src/components/BannerTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="text-rosybrown-800 flex flex-row justify-center text-5xl font-bold"
+  >
+    <RubyText :text="props.text" :yngping="props.yngping"></RubyText>
+  </div>
+</template>
+
+<script setup lang="ts">
+import RubyText from './RubyText.vue';
+
+const props = defineProps<{
+  text: string;
+  yngping: string;
+}>();
+</script>
