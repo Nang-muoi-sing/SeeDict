59c426bfa271ff3311c78589f19a49bb4acad2d7
Tseing <im.yczeng@foxmail.com>
2025-09-14 16:47:26 +0800

    refactor: About 页面应用新版设计

--- src/components/DashboardCard.vue
+++ src/components/DashboardCard.vue
@@ -2,48 +2,42 @@
   <div
     class="flex flex-col items-center justify-between gap-4 px-5 py-5 md:flex-row"
   >
-    <div
-      class="h-fill border-rosybrown-800 bg-rosybrown-50 w-48 rounded-xl border-1"
-    >
-      <div class="space-y-1 p-5">
+    <div class="h-fill border-rosybrown-800 bg-wheat-100 w-48 border-1">
+      <div class="space-y-5 px-5 py-3">
         <div class="flex flex-row items-center justify-between">
           <h3 class="text-rosybrown-700">平话词条</h3>
-          <i-material-symbols-dictionary-rounded
+          <i-material-symbols-description-rounded
             class="text-rosybrown-700"
             style="font-size: 16px"
           />
         </div>
-        <p class="text-rosybrown-800 text-2xl font-bold">22,748</p>
+        <p class="text-rosybrown-700 text-3xl font-bold">22,748</p>
       </div>
     </div>
 
-    <div
-      class="h-fill border-rosybrown-800 bg-rosybrown-50 w-48 rounded-xl border-1"
-    >
-      <div class="space-y-1 p-5">
+    <div class="h-fill border-rosybrown-700 bg-wheat-100 w-48 border-1">
+      <div class="space-y-5 px-5 py-3">
         <div class="flex flex-row items-center justify-between">
           <h3 class="text-rosybrown-700">榕拼条目</h3>
-          <i-material-symbols-voice-selection
+          <i-material-symbols-description-rounded
             class="text-rosybrown-700"
             style="font-size: 16px"
           />
         </div>
-        <p class="text-rosybrown-800 text-2xl font-bold">51,282</p>
+        <p class="text-rosybrown-700 text-3xl font-bold">51,282</p>
       </div>
     </div>
 
-    <div
-      class="h-fill border-rosybrown-800 bg-rosybrown-50 w-48 rounded-xl border-1"
-    >
-      <div class="space-y-1 p-5">
+    <div class="h-fill border-rosybrown-700 bg-wheat-100 w-48 border-1">
+      <div class="space-y-5 px-5 py-3">
         <div class="flex flex-row items-center justify-between">
           <h3 class="text-rosybrown-700">释义条目</h3>
-          <i-material-symbols-text-snippet-rounded
+          <i-material-symbols-description-rounded
             class="text-rosybrown-700"
             style="font-size: 16px"
           />
         </div>
-        <p class="text-rosybrown-800 text-2xl font-bold">4,201</p>
+        <p class="text-rosybrown-700 text-3xl font-bold">4,201</p>
       </div>
     </div>
   </div>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,77 +1,69 @@
 <template>
-  <div class="py-5 px-5">
-    <ol class="items-center sm:flex">
-      <li class="relative mb-6 sm:mb-0">
-        <div class="flex items-center">
+  <div class="px-5 py-5">
+    <ol class="sm:flex">
+      <li class="relative">
+        <div class="grid grid-cols-[auto_1fr] items-center gap-0">
           <div
-            class="bg-wheat-100 z-10 flex h-6 w-6 shrink-0 items-center justify-center rounded-full ring-0 ring-white sm:ring-8"
-          >
-            <i-material-symbols-waving-hand-rounded
-              class="text-rosybrown-800"
-              style="font-size: 14px"
-            />
-          </div>
-          <div class="bg-wheat-200 hidden h-0.5 w-full sm:flex"></div>
+            class="bg-wheat-50 border-wheat-300 z-10 h-2.5 w-2.5 shrink-0 rounded-full border-3"
+          ></div>
+          <div class="bg-wheat-200 hidden h-0.5 w-full sm:block"></div>
         </div>
-        <div class="mt-3 sm:pe-8">
-          <h3 class="text-rosybrown-900 text-lg font-semibold">
-            筹备蓝尾星小组
-          </h3>
-          <time
-            class="text-rosybrown-400 mb-2 block text-sm leading-none font-normal"
-            >2025 年初</time
-          >
-          <p class="text-base font-normal text-gray-500">
-            得知《榕典》运营上的变动，平话不能没有一个可用的线上词典，蓝尾星小组匆匆组织了起来
-          </p>
+        <div class="grid grid-cols-[auto_1fr] gap-4 sm:pe-8">
+          <div class="bg-wheat-200 ml-[4px] block h-full w-0.5 md:hidden"></div>
+          <div class="pt-0 pb-4 sm:pt-4 sm:pb-0">
+            <h3 class="text-wheat-600 text-lg font-semibold">筹备蓝尾星小组</h3>
+            <time
+              class="text-wheat-400 mb-2 block text-sm leading-none font-normal"
+              >2025 年初</time
+            >
+            <p class="text-wheat-500 text-base font-normal">
+              得知《榕典》运营上的变动，平话不能没有一个可用的线上词典，蓝尾星小组匆匆组织了起来
+            </p>
+          </div>
         </div>
       </li>
-      <li class="relative mb-6 sm:mb-0">
-        <div class="flex items-center">
+      <li class="relative">
+        <div class="grid grid-cols-[auto_1fr] items-center gap-0">
           <div
-            class="bg-wheat-100 z-10 flex h-6 w-6 shrink-0 items-center justify-center rounded-full ring-0 ring-white sm:ring-8"
-          >
-            <i-material-symbols-rocket-launch-rounded
-              class="text-rosybrown-800"
-              style="font-size: 14px"
-            />
-          </div>
-          <div class="bg-wheat-200 hidden h-0.5 w-full sm:flex"></div>
+            class="bg-wheat-50 border-wheat-300 z-10 h-2.5 w-2.5 shrink-0 rounded-full border-3"
+          ></div>
+          <div class="bg-wheat-200 hidden h-0.5 w-full sm:block"></div>
         </div>
-        <div class="mt-3 sm:pe-8">
-          <h3 class="text-rosybrown-900 text-lg font-semibold">
-            《米时典》正式上线
-          </h3>
-          <time
-            class="text-rosybrown-400 mb-2 block text-sm leading-none font-normal"
-            >2025 年 9 月 15 日</time
-          >
-          <p class="text-base font-normal text-gray-500">
-            在原有数据的基础上，我们终于赶制并发布了具有基本查询功能的《米时典》
-          </p>
+        <div class="grid grid-cols-[auto_1fr] gap-4 sm:pe-8">
+          <div class="bg-wheat-200 ml-[4px] block h-full w-0.5 md:hidden"></div>
+          <div class="pt-0 pb-4 sm:pt-4 sm:pb-0">
+            <h3 class="text-wheat-600 text-lg font-semibold">
+              《米时典》正式上线
+            </h3>
+            <time
+              class="text-wheat-400 mb-2 block text-sm leading-none font-normal"
+              >2025 年 9 月 15 日</time
+            >
+            <p class="text-wheat-500 text-base font-normal">
+              在原有数据的基础上，我们终于赶制并发布了具有基本查询功能的《米时典》
+            </p>
+          </div>
         </div>
       </li>
-      <li class="relative mb-6 sm:mb-0">
-        <div class="flex items-center">
+      <li class="relative">
+        <div class="grid grid-cols-[auto_1fr] items-center gap-0">
           <div
-            class="bg-wheat-100 z-10 flex h-6 w-6 shrink-0 items-center justify-center rounded-full ring-0 ring-white sm:ring-8"
-          >
-            <i-material-symbols-star-shine-rounded
-              class="text-rosybrown-800"
-              style="font-size: 14px"
-            />
-          </div>
-          <div class="bg-wheat-200 hidden h-0.5 w-full sm:flex"></div>
+            class="bg-wheat-50 border-wheat-300 z-10 h-2.5 w-2.5 shrink-0 rounded-full border-3"
+          ></div>
+          <div class="bg-wheat-200 hidden h-0.5 w-full sm:block"></div>
         </div>
-        <div class="mt-3 sm:pe-8">
-          <h3 class="text-rosybrown-900 text-lg font-semibold">更长的前路</h3>
-          <time
-            class="text-rosybrown-400 mb-2 block text-sm leading-none font-normal"
-            >未来</time
-          >
-          <p class="text-base font-normal text-gray-500">
-            音频数据的整理、更多的词汇、更多地域的读音，我们还有很长的路要走……
-          </p>
+        <div class="grid grid-cols-[auto_1fr] gap-4 sm:pe-8">
+          <div class="bg-wheat-200 ml-[4px] block h-full w-0.5 md:hidden"></div>
+          <div class="pt-0 pb-4 sm:pt-4 sm:pb-0">
+            <h3 class="text-wheat-600 text-lg font-semibold">更长的前路</h3>
+            <time
+              class="text-wheat-400 mb-2 block text-sm leading-none font-normal"
+              >未来</time
+            >
+            <p class="text-wheat-500 text-base font-normal">
+              音频数据的整理、更多的词汇、更多地域的读音，我们还有很长的路要走……
+            </p>
+          </div>
         </div>
       </li>
     </ol>
