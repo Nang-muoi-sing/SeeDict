aee5837325254d0e6c8fde6752206528d1d3c257
Tseing <im.yczeng@foxmail.com>
2025-09-14 16:47:26 +0800

    refactor: About 页面应用新版设计

--- src/components/SpeechBubble.vue
+++ src/components/SpeechBubble.vue
@@ -1,25 +1,29 @@
 <template>
-  <div class="flex flex-col">
-    <div class="rounded-xl px-5 py-3" :class="props.bubbleClass">
-      <slot></slot>
+  <div>
+    <div class="flex flex-col">
+      <div class="rounded-xl px-5 py-3" :class="props.bubbleClass">
+        <slot></slot>
+      </div>
+      <div class="flex flex-row" :class="{ 'justify-end': !props.leftArrow }">
+        <div
+          class="relative mx-5 h-3 w-7 border-t-8 border-r-14 border-b-8 border-l-14 border-r-transparent border-b-transparent border-l-transparent"
+          :class="props.arrowClass"
+        ></div>
+      </div>
     </div>
-    <div class="flex flex-row" :class="{ 'justify-end': !props.leftArrow }">
-      <div
-        class="relative mx-5 h-3 w-7 border-t-8 border-r-14 border-b-8 border-l-14 border-r-transparent border-b-transparent border-l-transparent"
-        :class="props.arrowClass"
-      ></div>
-    </div>
-  </div>
-  <div v-if="avatarUrl" class="relative ml-[.875rem]">
-    <div class="flex flex-row items-center space-x-3">
-      <img
-        class="h-10 w-10 rounded-full border-4 border-white object-cover"
-        :src="avatarUrl"
-        alt="Avatar"
-      />
-      <template v-if="nickname">
-        <span class="text-md font-semibold text-zinc-600">{{ nickname }}</span>
-      </template>
+    <div v-if="avatarUrl" class="relative ml-[.875rem]">
+      <div class="flex flex-row items-center space-x-3">
+        <img
+          class="h-10 w-10 rounded-full border-4 border-white object-cover"
+          :src="avatarUrl"
+          alt="Avatar"
+        />
+        <template v-if="nickname">
+          <span class="text-md font-semibold text-zinc-600">{{
+            nickname
+          }}</span>
+        </template>
+      </div>
     </div>
   </div>
 </template>
--- src/components/SpeechBubbles.vue
+++ src/components/SpeechBubbles.vue
@@ -1,33 +1,38 @@
 <template>
   <div>
     <SpeechBubble
-      :arrow-class="'border-t-rosybrown-100'"
-      :bubble-class="'bg-rosybrown-100'"
-      ><p class="text-rosybrown-900 text-sm">
+      class="relative before:absolute before:-top-3 before:left-3 before:content-['➊']"
+      :arrow-class="'border-t-wheat-100'"
+      :bubble-class="'bg-wheat-100'"
+    >
+      <p class="text-wheat-600 text-sm">
         𬖞是福州人在冬至时制作的寓意团圆的美食，具有福州腔调，也很有榕城的烟火气
       </p></SpeechBubble
     >
     <SpeechBubble
+      class="relative before:absolute before:-top-3 before:left-3 before:content-['➋']"
       :left-arrow="false"
       :arrow-class="'border-t-wheat-100'"
       :bubble-class="'bg-wheat-100'"
-      ><p class="text-rosybrown-900 text-sm">
+      ><p class="text-wheat-600 text-sm">
         和做玻当𬖞一样，需要社会上每一分子都为福州话保护添上「芝麻」「豆粉」「白砂糖」，
         才能做大做强，把福州话保护好
       </p></SpeechBubble
     >
     <SpeechBubble
-      :arrow-class="'border-t-rosybrown-200'"
-      :bubble-class="'bg-rosybrown-200'"
-      ><p class="text-rosybrown-900 text-sm">
+      class="relative before:absolute before:-top-3 before:left-3 before:content-['➌']"
+      :arrow-class="'border-t-wheat-100'"
+      :bubble-class="'bg-wheat-100'"
+      ><p class="text-wheat-600 text-sm">
         「𬖞」是在街头巷尾常见的方言字，我们的一大愿望也是让大家了解到我们的方言是能够写出来的
       </p></SpeechBubble
     >
     <SpeechBubble
+      class="relative before:absolute before:-top-3 before:left-3 before:content-['➍']"
       :left-arrow="false"
-      :arrow-class="'border-t-wheat-200'"
-      :bubble-class="'bg-wheat-200'"
-      ><p class="text-rosybrown-900 text-sm">
+      :arrow-class="'border-t-wheat-100'"
+      :bubble-class="'bg-wheat-100'"
+      ><p class="text-wheat-600 text-sm">
         「𬖞」和英文的 see
         谐音，在平话和普通话里，「𬖞典」又和「词典」谐音，我们就是一部希望平话被更多人看见的词典
       </p></SpeechBubble
