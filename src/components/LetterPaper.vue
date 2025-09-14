e7a99b6e3de8d2889173a4a28db7c24032e9ee78
Tseing <im.yczeng@foxmail.com>
2025-09-14 16:47:26 +0800

    refactor: About 页面应用新版设计

---
+++ src/components/LetterPaper.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-white relative">
+        <img class="absolute right-7 -top-3" src="../assets/paper-clip.svg"></img>
+        <div class="py-8 px-10">
+            <slot></slot>
+        </div>
+    </div>
+</template>
