319bf3b5786409e6f9205538a24047265c73a854
Tseing <im.yczeng@foxmail.com>
2025-09-25 00:14:59 +0800

    refactor(ui): 优化词汇内容中的图标对齐

--- src/components/SeeSymbol.vue
+++ src/components/SeeSymbol.vue
@@ -1,5 +1,12 @@
 <template>
-  <span class="seeicon- mr-1 align-text-bottom" style="font-size: 20px; display: inline-block; width: 1em;"
+  <span
+    class="seeicon- mr-1"
+    style="
+      font-size: 20px;
+      display: inline-block;
+      width: 1em;
+      vertical-align: -0.1em;
+    "
     ><slot></slot
   ></span>
 </template>
