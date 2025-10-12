bd9d7681bc4335ffcab844aac594be7c74377343
Tseing <im.yczeng@foxmail.com>
2025-10-12 11:31:34 +0800

    fix(ui): #52 复制词头的动画适配移动端

--- src/components/WordHead.vue
+++ src/components/WordHead.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative z-10 mb-8 mt-5 cursor-pointer overflow-hidden rounded-lg bg-wheat-100 px-8 py-6 transition-all duration-300 ease-in-out before:absolute before:left-[100%] before:top-[100%] before:-z-10 before:h-[100rem] before:w-[100rem] before:rounded-full before:bg-wheat-200 before:transition-all before:duration-700 before:content-[''] hover:before:-left-[16rem] hover:before:-top-[16rem] active:before:transition-colors active:before:duration-0"
+    class="relative z-10 mb-8 mt-5 cursor-pointer overflow-hidden rounded-lg bg-wheat-100 px-8 py-6 transition-all duration-300 ease-in-out before:absolute before:left-[100%] before:top-[100%] before:-z-10 before:h-[100rem] before:w-[100rem] before:rounded-full before:bg-wheat-200 before:transition-all before:duration-700 before:content-[''] active:before:-left-[16rem] active:before:-top-[16rem] active:before:transition-colors active:before:duration-0 md:hover:before:-left-[16rem] md:hover:before:-top-[16rem]"
     :class="isAudioClicking ? '' : 'active:scale-95'"
     @click.prevent="handleCopyClick"
   >
@@ -28,15 +28,17 @@
       </div>
     </div>
   </div>
+  <div
+    ref="copyTip"
+    class="fixed bottom-0 left-0 right-0 z-50 -mb-12 flex justify-center"
+  >
     <div
-      ref="copyTip"
-      class="fixed bottom-0 left-0 right-0 z-50 flex justify-center -mb-12"
+      class="flex flex-row items-center gap-1 rounded-lg bg-white px-6 py-3 font-medium text-wheat-800 shadow-lg"
     >
-      <div class=" flex-row flex items-center gap-1 rounded-lg bg-white px-6 py-3 font-medium text-wheat-800 shadow-lg">
-        <i-material-symbols-check-circle-rounded class="text-green-600" />
-        已复制词条
-      </div>
+      <i-material-symbols-check-circle-rounded class="text-green-600" />
+      已复制词条
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
