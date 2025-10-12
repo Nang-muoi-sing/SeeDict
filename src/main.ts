ff23f940cc9e165c7f1904874da52aeab9859431
Tseing <im.yczeng@foxmail.com>
2025-10-12 23:16:46 +0800

    feat: 引入全局状态避免页面后退时词卡的重复请求

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
 import './style.css';
 
-import router from './router';
-
 const app = createApp(App);
+const pinia = createPinia();
 app.use(router);
+app.use(pinia);
 app.mount('#app');
